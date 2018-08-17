e9af35192f43f03e9a31a40d6410be1d6ec90116
Romain Dartigues <romain-dartigues@users.noreply.github.com>
2018-08-17 10:26:06 +0200

    Initial commit

---
+++ CustomFieldsLinks.php
@@ -0,0 +1,153 @@
+<?php
+
+/**
+ * Custom fields links plugin
+ */
+class CustomFieldsLinksPlugin extends MantisPlugin {
+	const VERSION = '0.0.1';
+	const SEPARATOR = ', ';
+	const R_FIELD = '/^([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*(.+)\s*$/m';
+
+	protected $fields = array();
+	protected $separator = NULL;
+
+	/**
+	 * A method that populates the plugin information and minimum requirements.
+	 * @return void
+	 */
+	function register()
+	{
+		$this->name = lang_get('plugin_CustomFieldsLinks_name');
+		$this->description = lang_get('plugin_CustomFieldsLinks_description');
+		$this->page = 'config';
+		$this->version = self::VERSION;
+		$this->requires = array(
+			'MantisCore' => '1.3.5',
+			// requiring https://github.com/mantisbt/mantisbt/pull/655?
+		);
+		$this-> uses = array();
+		$this->author = 'Romain Dartigues';
+		$this->contact = '';
+		$this->url = 'https://github.com/romain-dartigues/mantisbt-customfieldslinks';
+
+	}
+
+	/**
+	 * Default plugin configuration.
+	 * @return array
+	 */
+	function config()
+	{
+		return array(
+			'separator' => self::SEPARATOR,
+			'fields' => NULL,
+		);
+	}
+
+	function hooks()
+	{
+		return array(
+			'EVENT_DISPLAY_FORMATTED' => 'display_formatted',
+			'EVENT_PLUGIN_INIT' => 'get_configuration',
+		);
+	}
+
+	function get_configuration()
+	{
+		$this->fields = self::parse_fields(
+			plugin_config_get('fields')
+		);
+		$this->separator = plugin_config_get('separator');
+	}
+
+	/**
+	 * Hopefully convert a list of elements to a list of links
+	 *
+	 * @param string $p_event
+	 * @param string $p_chained_param
+	 * @print Formatted text
+	 * @return NULL
+	 */
+	function display_formatted($p_event, $p_chained_param)
+	{
+		$t_args = NULL;
+		$t_data = array();
+		$t_return = array();
+		$t_url_format = '%s';
+
+		// find if we are called by the custom field API
+		foreach(debug_backtrace() as $t_row)
+		{
+			if ($t_row['function'] == 'string_custom_field_value')
+			{
+				$t_args = $t_row['args'][0];
+				break;
+			}
+		}
+
+		if (
+			!$t_args
+			or
+			!$p_chained_param
+			or
+			$p_event != 'EVENT_DISPLAY_FORMATTED'
+			or
+			!(
+				empty($t_args['valid_regexp'])
+				or
+				preg_match_all(
+					"/${t_args['valid_regexp']}/",
+					$p_chained_param,
+					&$t_data
+				)
+			)
+		)
+		{
+			return $p_chained_param;
+		}
+
+		// generate the links
+		if (!empty($this->fields[ $t_args['name'] ]))
+		{
+			$t_url_format =&$this->fields[ $t_args['name'] ];
+		}
+
+		foreach($t_data[0] as $t_val)
+		{
+			$t_return[] = sprintf(
+				'<a href="' . $t_url_format .'">%s</a>',
+				urlencode($t_val),
+				htmlspecialchars($t_val)
+			);
+		}
+
+		/*
+		 * Work around plugin MantisCoreFormatting messing with
+		 * the output when either "text" or "url" processing is enabled.
+		 * Print the result and return NULL to stop further processing.
+		 * See: plugin.php?page=MantisCoreFormatting/config
+		 */
+		echo implode($this->separator, $t_return);
+
+		return NULL;
+	}
+
+	/**
+	 * Extract fields from a string
+	 *
+	 * @param string $input
+	 * @return array or FALSE
+	 */
+	function parse_fields($input)
+	{
+		$data = array();
+		if (preg_match_all(self::R_FIELD, $input, $data))
+		{
+			return array_combine($data[1], $data[2]);
+		}
+		return FALSE;
+	}
+
+}
+
+?>
---
+++ pages/config.php
@@ -0,0 +1,93 @@
+<?php
+auth_reauthenticate();
+access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
+
+/*
+ * Form handling
+ */
+$t_errors = array();
+
+
+/**
+ * workaround config_eval() called by plugin_config_get()
+ *
+ * This function exist because I did not understand how to deal with
+ * the function `config_eval()` and it's evaluation of lines with multiple percent sign.
+ *
+ * @see config_eval()
+ * @see plugin_config_set()
+ * @return void
+ */
+function workaround_plugin_config_set($p_option, $p_value)
+{
+	return plugin_config_set(
+		$p_option,
+		str_replace( '%', '\\%', $p_value )
+	);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$t_fields = gpc_get_string('fields', NULL);
+	if (!CustomFieldsLinksPlugin::parse_fields($t_fields))
+	{
+		$t_errors[] = 'fields';
+	}
+
+	if (empty($t_errors))
+	{
+		workaround_plugin_config_set(
+			'separator',
+			gpc_get_string(
+				'separator',
+				CustomFieldsLinksPlugin::SEPARATOR
+			)
+		);
+		workaround_plugin_config_set(
+			'fields',
+			$t_fields
+		);
+		header('Location: ' . plugin_page( 'config',TRUE ));
+		print_successful_redirect( plugin_page( 'config',TRUE ) );
+	}
+}
+
+
+html_page_top1( 'Custom fields links' );
+html_page_top2();
+print_manage_menu();
+?>
+
+<form action="<?php echo plugin_page( 'config' ) ?>" method="post">
+
+<div class="form-group">
+ <label for="separator"><?php echo lang_get( 'plugin_CustomFieldsLinks_separator' ); ?></label>
+ <input type="text" name="separator" id="separator" value="<?php echo htmlspecialchars(plugin_config_get( 'separator' ));?>">
+ <small><?php printf(
+  lang_get( 'plugin_CustomFieldsLinks_separator_help' ),
+  htmlspecialchars(CustomFieldsLinksPlugin::SEPARATOR)
+  ); ?></small>
+</div>
+
+<div class="form-group<?php if (in_array('fields', $t_errors)) { echo ' has-error'; } ?>">
+ <label for="fields"><?php echo lang_get( 'plugin_CustomFieldsLinks_fields' ); ?></label><br>
+ <small>
+  <?php printf(
+   lang_get( 'plugin_CustomFieldsLinks_fields_help' ),
+   'manage_custom_field_page.php',
+   'http://php.net/sprintf'
+  ); ?>
+ </small><br><?php if (in_array('fields', $t_errors)):?>
+ <span class="help-block"><?php echo lang_get( 'plugin_CustomFieldsLinks_invalid_format' ); ?></span><br><?php endif; ?>
+ <textarea name="fields" id="fields" placeholder="FieldName = https://mantisbt.example.net/view.php?id=%d" cols="80" rows="5" maxlength=""><?php echo htmlspecialchars(plugin_config_get( 'fields' )); ?></textarea>
+</div>
+
+<div class="form-group">
+ <input type="submit" class="button" value="<?php echo lang_get( 'plugin_update' ) ?>">
+</div>
+
+</form>
+
+<?php
+html_page_bottom1( __FILE__ );
+?>
